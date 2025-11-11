27d075befe0932850e0cf4cdfe6f62856a07eeb4
Miyu K <miyu.kobayashi329@gmail.com>
2025-11-11 15:53:13 -0500

    2025-11-11

--- expense-tracker/src/Components/Balance.js
+++ expense-tracker/src/Components/Balance.js
@@ -2,7 +2,7 @@ import React, { useContext} from "react";
 import { GlobalContext} from '../context/GlobalState';
 
 export const Balance = () => {
-    const { transaction } = useContext(GlobalContext);
+    const { transactions } = useContext(GlobalContext);
 
     const amounts = transactions.map(transaction => transaction.amount);
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
