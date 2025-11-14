4abe1c0d214c351220fe0a7095caafdcf1193b26
Miyu K <miyu.kobayashi329@gmail.com>
2025-11-14 06:32:56 -0500

    2025-11-14

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,20 +1,76 @@
-import React from 'react';
-import { GlobalProvider } from './context/GlobalState';
-import { Balance } from './Components/Balance';
+import { useState } from 'react';
+import TodoInput from './componets.TodoInput';
+import TodoList from './componets/TodoList';
 
-function App() {
-  return (
-    <GlobalProvider>
-      <div className='container'>
-        <h2>Expence Tracker (Practice)</h2>
-        <Balance />
-      </div>
-    </GlobalProvider>
-  );
-}
+export default function App () {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+setTodo = ([...todos, { id: Date.now(), text, completed: false}]);
+  };
+
+  cont toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+      todo.id === id ? {...todo, completed: !todo.completed } : todo)
+    );
+  };
+};
 
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { GlobalProvider } from './context/GlobalState';
+// import { Balance } from './Components/Balance';
+
+// function App() {
+//   return (
+//     <GlobalProvider>
+//       <div className='container'>
+//         <h2>Expence Tracker (Practice)</h2>
+//         <Balance />
+//       </div>
+//     </GlobalProvider>
+//   );
+// }
+
+
+// export default App;
 
 
 
