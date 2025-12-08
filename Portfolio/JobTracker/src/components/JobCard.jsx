950c3fcd53fe64c5b55ac2f8c4e9b676870ad8ad
Miyu K <miyu.kobayashi329@gmail.com>
2025-12-08 06:09:58 -0500

    2025-12-8

--- Portfolio/JobTracker/src/components/JobCard.jsx
+++ Portfolio/JobTracker/src/components/JobCard.jsx
@@ -1,19 +1,55 @@
+import { useJobs } from "../context/JobContext";
 import './JobCard.css';
 
 function JobCard ({ job }) {
+    const { updateJobStatus, removeJob } = useJobs();
+
     return (
     <div className='job-card'>
         <div className='job-card-header'>
-            <h3>{job.position}</h3>
-            <span className={`status-badge ${job.status}`}>
-                {job.status}
+        <h3>{job.position}</h3>
+        <p className="company-name">{job.company}</p>
+
+            <span className={`status ${job.status}`}>
+            {job.status}
             </span>
         </div>
 
-        <p className='company-name'>{job.company}</p>
-    </div>    
-    );
+        <div className="job-card-actions">
+            <select
+            className="status-select"
+            value={job.status}
+            onChange={(e) => updateStatus(job.id, e.target.value)}
+            >
+              <option value="saved">Saved</option>
+              <option value="applied">Applied</option>
+              <option value="Interview">Interview</option>
+              <option value="offer">Offer</option>
+              <option value="rejected">Rejected</option>
+            </select>
+            
+             <button className='delete-btn'
+               onClick={() => removeJob(job.id)}
+               >
+               Delete
+            </button>
+          </div>
+        </div>
+      );
 }
 
 
 export default JobCard;
+
+
+
+
+    //     {/* <div className='job-card-header'>
+    //         <h3>{job.position}</h3>
+    //         <span className={`status-badge ${job.status}`}>
+    //             {job.status}
+    //         </span>
+    //     </div>
+
+    //     <p className='company-name'>{job.company}</p>
+    // </div>     */}
