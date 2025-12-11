f783d5c2a18ddbfbf79c0ffdbebdd339a5c1ecac
Miyu K <miyu.kobayashi329@gmail.com>
2025-12-11 17:32:32 -0500

    2025-12-11

--- Portfolio/JobTracker/src/pages/JobListPage.jsx
+++ Portfolio/JobTracker/src/pages/JobListPage.jsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+// pages/JobListPage.jsx
+import AddJobForm from "../components/AddJobForm";
 import { useJobs } from "../context/JobContext";
 import JobCard from "../components/JobCard";
-import './JobListPage.css';
+import "./JobListPage.css";
 
 function JobListPage() {
   const { jobs } = useJobs();
 
-return (
-  <div className="job-list-container">
-    <h2>Job List</h2>
-    <div className="job-list">
-      {jobs.length === 0 && <p>No Jobs added yet.</p>}
-      {jobs.map(job => (
-        <jobCard key={job.id} job={job} />
-      ))}
-  </div>
-</div>
-);
+  return (
+    <div className="job-list-container">
+      <h1>Job Tracker</h1>
+
+      <h2>Add Job</h2>
+      <AddJobForm />
+
+      <h2>Job List</h2>
+      <div className="job-list">
+        {jobs.length === 0 && <p>No jobs added yet.</p>}
+
+        {jobs.map((job) => (
+          <JobCard key={job.id} job={job} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default JobListPage;
@@ -44,57 +51,9 @@ export default JobListPage;
   // };
 
 
-  //   const [company, setCompany] = useState('');
-//   const [position, setPosition] = useState('');
-//   const [status, setStatus] = useState('saved');
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
+  //   
 //       // addJob(company, position, status);
 
-//     const newJob = {
-//     id: Date.now(),
-//     company,
-//     position,
-//     status,
-//   };
 
-//   addJob(newJob);
 
-//   setCompany('');
-//   setPosition('');
-//   setStatus('saved');
-// };
-
-//   return (
-//     <div className='job-list-container'>
-//       <h1>Job Tracker</h1>
-
-//       <h2>Add Job</h2>
-//       <form onSubmit={handleSubmit} className="job-form">
-//         <input 
-//           type="text" 
-//           placeholder="Company" 
-//           value={company}
-//           onChange={(e) => setCompany(e.target.value)}
-//           required
-//         />
-
-//         <input 
-//           type="text" 
-//           placeholder="Position" 
-//           value={position}
-//           onChange={(e) => setPosition(e.target.value)}
-//           required
-//         />
-
-//         <select value={status} onChange={(e) => setStatus(e.target.value)}>
-//           <option value='saved'>Saved</option>
-//           <option value='applied'>Applied</option>
-//           <option value='interview'>Interview</option>
-//           <option value='offer'>Offer</option>
-//           <option value='rejected'>Rejected</option>
-//         </select>
-
-//         <button type="submit">Add</button>
-//       </form>
+//  
