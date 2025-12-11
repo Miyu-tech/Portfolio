e08e28fa5cb1254778aa5e78e1a1e5e0ceb1d8c9
Miyu K <miyu.kobayashi329@gmail.com>
2025-12-11 17:32:59 -0500

    2025-12-11

--- Portfolio/JobTracker/src/components/JobCard.jsx
+++ Portfolio/JobTracker/src/components/JobCard.jsx
@@ -1,45 +1,39 @@
 import { useJobs } from "../context/JobContext";
-import './JobCard.css';
+import "./JobCard.css";
 
-function JobCard ({ job }) {
-    const { updateJobStatus, removeJob } = useJobs();
+function JobCard({ job }) {
+  const { updateJobStatus, removeJob } = useJobs();
 
-    return (
-    <div className='job-card'>
-        <div className='job-card-header'>
-        <h3>{job.position}</h3>
-        <p className="company-name">{job.company}</p>
+  return (
+    <div className="job-card">
+      <h3>{job.position}</h3>
+      <p className="company-name">{job.company}</p>
 
-            <span className={`status ${job.status}`}>
-            {job.status}
-            </span>
-        </div>
+      <span className={`status ${job.status}`}>{job.status}</span>
 
-        <div className="job-card-actions">
-            <select
-            className="status-select"
-            value={job.status}
-            onChange={(e) => updateStatus(job.id, e.target.value)}
-            >
-              <option value="saved">Saved</option>
-              <option value="applied">Applied</option>
-              <option value="Interview">Interview</option>
-              <option value="offer">Offer</option>
-              <option value="rejected">Rejected</option>
-            </select>
-            
-             <button className='delete-btn'
-               onClick={() => removeJob(job.id)}
-               >
-               Delete
-            </button>
-          </div>
-        </div>
-      );
+      <div className="job-card-actions">
+        <select
+          className="status-select"
+          value={job.status}
+          onChange={(e) => updateJobStatus(job.id, e.target.value)}
+        >
+          <option value="saved">Saved</option>
+          <option value="applied">Applied</option>
+          <option value="interview">Interview</option>
+          <option value="offer">Offer</option>
+          <option value="rejected">Rejected</option>
+        </select>
+
+        <button className="delete-btn" onClick={() => removeJob(job.id)}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
-
 export default JobCard;
+
 
 
 
