30bb5003d73e5fff2418a0948567302390b6b831
Miyu K <miyu.kobayashi329@gmail.com>
2025-12-17 17:27:53 -0500

    2025-12-17

--- Portfolio/JobTracker/src/App.jsx
+++ Portfolio/JobTracker/src/App.jsx
@@ -1,6 +1,12 @@
-import JobListPage from './pages/JobListPage';
+import { JobProvider } from "./context/JobContext";
+import JobListPage from "./pages/JobListPage";
 
-function App(){
-  return <JobListPage />
+function App() {
+  return (
+    <JobProvider>
+      <JobListPage />
+    </JobProvider>
+  );
 }
+
 export default App;
